4a7b74dbde3205e5fe2ba5f96b799ef382cb8b86
Utsav23sha <50926618+Utsav23sha@users.noreply.github.com>
2021-03-19 13:26:31 +0530

    Delete pages directory

--- pages/Accessories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Accesories() {
-  return <h1 className='accessories'>ACCESSORIES</h1>;
-}
--- pages/Clothing.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Clothing() {
-  return <h1 className='clothing'>CLOTHING</h1>;
-}
